01d07bcdcffe47a6e47f5c417fcadb2c2facc787
Alexis Brones <abrones-ext@splio.com>
2021-10-14 19:20:19 +0200

    First commit: initiate project vue default VueCli for Vue3 and start homepage

---
+++ src/components/Home.vue
@@ -0,0 +1,14 @@
+<template lang="fr">
+  <div>
+    <h1>MyNd Sandbox</h1>
+    <h3>My developer sandbox for experiment and have fun.</h3>
+  </div>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="fr">
+  
+</style>
