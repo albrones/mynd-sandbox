d92f97d0cfb2672204a1e0c4a91990dec04bf890
Alexis Brones <abrones-ext@splio.com>
2021-10-24 19:28:34 +0200

    Impl basic DailyRoutine

---
+++ src/components/base/TodoItem.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <input
+      class="todo-item"
+      type="checkbox"
+      :id="id"
+      :name="name"
+      :checked="checked"
+      @click="$emit('updateTodoStatus', { id, name, checked })"
+    />
+    <label :for="id"> {{ name }}</label>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  props: {
+    id: Number,
+    name: String,
+    checked: Boolean,
+  },
+  emits: ["updateTodoStatus"],
+}
+</script>
